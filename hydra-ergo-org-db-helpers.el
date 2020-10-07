74c4413eb6d382624b38f176c8dad7e8be93be3c
Olle Svensson <Olle.Svensson@hoganas.com>
2020-10-07 15:44:35 +0200

    split hydra-ergo-org-to-vars to two functions

--- hydra-ergo-org-db-helpers.el
+++ hydra-ergo-org-db-helpers.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: nil; -*-
+
 (defun hydra-ergo-org-to-vars (fname)
   "Go through org file and create variables with names from
 from top headline containing at list with names of all subheadings"
@@ -6,18 +8,20 @@ from top headline containing at list with names of all subheadings"
   ;; (interactive)
   (let (vname group)
     (org-map-entries
-     '(progn
-	(move-end-of-line nil)
-	(if (equal (car (org-heading-components)) 1)
-	    (progn
-	      (org-show-children)
-	      (when vname
-		(set (intern vname) group)
-		(setq group nil))
-	      (setq vname (current-word)))
-	  (push (current-word) group))))
+     'hydra-ergo-parse-org-entry)
     (set (intern vname) group))
   (kill-buffer))
+
+(defun hydra-ergo-parse-org-entry ()
+    (move-end-of-line nil)
+    (if (equal (car (org-heading-components)) 1)
+	(progn
+	  (org-show-children)
+	  (when vname
+	    (set (intern vname) group)
+	    (setq group nil))
+	  (setq vname (current-word)))
+      (push (current-word) group)))
     
 (defun hydra-ergo-add-var-orgfile (fname parent entry)
   (find-file fname)
@@ -30,3 +34,4 @@ from top headline containing at list with names of all subheadings"
   (save-buffer)
   ;; (kill-buffer)
   (widen))
+
